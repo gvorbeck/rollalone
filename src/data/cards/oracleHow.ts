3c66f51051df17f8107be97b71a05cb2ba62c381
Garrett Vorbeck <gvorbeck@gmail.com>
2025-07-20 21:39:58 -0400

    refactor: restructure content property in oracleHow for improved organization

--- src/data/cards/oracleHow.ts
+++ src/data/cards/oracleHow.ts
@@ -6,16 +6,28 @@ const oracleHow: CardProps = {
   contentType: "table",
   preContent:
     "When you need to know how big, good, strong, numerous, etc. something is.",
-  content: {
-    title: "Answer (d6)",
-    rows: [
-      ["1", "Surprisingly lacking"],
-      ["2", "Less than expected"],
-      ["3-4", "About average"],
-      ["5", "More than expected"],
-      ["6", "Extraordinary"],
-    ],
-  },
+  content: [
+    {
+      title: "Quality (d6)",
+      rows: [
+        ["1", "Surprisingly lacking"],
+        ["2", "Less than expected"],
+        ["3-4", "About average"],
+        ["5", "More than expected"],
+        ["6", "Extraordinary"],
+      ],
+    },
+    {
+      title: "Reaction (d6)",
+      rows: [
+        ["1", "KILL!"],
+        ["2-3", "Angered"],
+        ["4", "Indifferent"],
+        ["5", "Almost Friendly"],
+        ["6", "Helpful"],
+      ],
+    },
+  ],
 };
 
 export default oracleHow;
